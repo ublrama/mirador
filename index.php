d5745036a926a78312721147d1af2b55827f7e0c
ublrama <r.k.s.mwinyimbegu@library.leidenuniv.nl>
2020-09-14 16:20:12 +0200

    Load selected CanvasID

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 
+header('Set-Cookie: same-site-cookie=foo; SameSite=Lax', false);
+header('Set-Cookie: cross-site-cookie=bar; SameSite=None; Secure', true);
+
 $manifest = "";
+$canvasID = "";
 $collection = "";
 if (isset($_GET["manifest"])) {
   $manifest = $_GET["manifest"];
+}
+if (isset($_GET["canvasID"])) {
+  $canvasID = $_GET["canvasID"];
 }
 
 
@@ -36,6 +43,7 @@ if (isset($_GET["collection"])) {
 <script type="text/javascript">
 
     var manifest = "<?php echo $manifest ?>";
+    var canvasID = "<?php echo $canvasID ?>";
     var collection = "<?php echo $collection ?>";
     var manifestdata = {manifestType: manifest};
     var type, uri, openManifestsPage;
@@ -43,12 +51,13 @@ if (isset($_GET["collection"])) {
     var preload = document.getElementById("preload");
     var user = document.getElementById("user");
 
-    if (manifest != "") {
+    if (manifest !== "") {
         type = {manifestUri: manifest, location: 'University Leiden Library'}
         uri = manifest;
         openManifestsPage = false;
     }
-    if (collection != "") {
+
+    if (collection !== "") {
 
         //user.style.display = "none";
         type = {collectionUri: collection, location: 'University Leiden Library'}
@@ -57,9 +66,9 @@ if (isset($_GET["collection"])) {
     }
 
 
-    if (manifest != "")
-        LoadManifestMirador(type, uri, openManifestsPage);
-    else if (collection != "")
+    if (manifest !=="")
+        LoadManifestMirador(type, uri, openManifestsPage,canvasID);
+    else if (collection !== "")
         LoadMCollectionMirador(type, uri, openManifestsPage);
     else
         LoadDefaultMirador();
@@ -71,90 +80,8 @@ if (isset($_GET["collection"])) {
             buildPath: "mirador/",
             preserveManifestOrder: true,
             data: [
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:29758/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:29759/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:29760/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53835/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16557/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16564/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16576/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16551/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:13085/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16641/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16644/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16669/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16608/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16610/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16580/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16687/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16692/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16679/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27506/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27507/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27509/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27532/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27533/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27536/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27519/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27522/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27523/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:29756/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:27535/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53915/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53912/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53910/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53911/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53914/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53918/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53916/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53917/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53993/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53994/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53995/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53913/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:17073/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:13102/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53402/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53415/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53409/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53520/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53473/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53559/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53502/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53505/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53781/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53541/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53545/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53571/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53493/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53496/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53509/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53512/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53515/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53462/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53816/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16700/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53399/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:26699/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:16869/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:26491/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:17146/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53711/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53706/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53703/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53728/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53716/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53719/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53725/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53722/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53889/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53892/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53895/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53879/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53884/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53861/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:17145/manifest' , location: 'University Leiden'},
-                { manifestUri:'https://scharrelaar-a3.leidenuniv.nl/iiif_manifest/item:53862/manifest' , location: 'University Leiden'},
+                { manifestUri:'https://localhost/iiif/?manifest=caribbean' , location: 'Local'},
+                //{ manifestUri:'http://localhost:8000//iiif_manifest/islandora:9/manifest' , location: 'Islandora'},
 
             ],
             manifestsPanel: {
@@ -191,7 +118,7 @@ if (isset($_GET["collection"])) {
         });
     }
 
-    function LoadManifestMirador(type, uri, openManifestsPage) {
+    function LoadManifestMirador(type, uri, openManifestsPage,canvasID) {
 
         myMiradorInstance = Mirador({
             id: "viewer",
@@ -209,6 +136,7 @@ if (isset($_GET["collection"])) {
             windowObjects: [{
                 sidePanel: false,
                 loadedManifest: uri,
+                canvasID : canvasID,
                 annotationLayer: false
             }],
             /*annotationEndpoint: {
